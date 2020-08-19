3ec4f7aff7c5c109dc0462ec56041b3af0ecfccd
김 남규 <skarb629@naver.com>
2020-08-20 00:24:25 +0900

    header nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Header from './Component/Header';
 function App() {
   return (
-    <div className="App">
-      test
+    <div style={{margin:0, padding:0}}>
+      <Header/>
     </div>
   );
 }
