e3bcabf2468aab1133019070ed1f3f765350cafd
namkyu kim <southkyu@gmail.com>
2020-09-15 11:16:37 +0900

    Project 완료 Skill Start

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Image, Card, Tag, Avatar } from 'antd';
+import { Layout, Image, Card, Tag, Divider } from 'antd';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import { GithubOutlined } from '@ant-design/icons';
@@ -27,9 +27,18 @@ function Project(props) {
             content: 'Pos 프로그램 개발',
             tag: ['C#', 'Winform', '.Net Core WebAPI', 'MariaDB'],
             git: 'https://github.com/KimNamKyu/mojavePos',
-            text: '팀프로젝트 Pos시스템개발( 주문 프로세스 개발, 메뉴 테이블 설계 및 공통모듈화 담당)',
+            text: '팀프로젝트 Pos시스템개발( 주문 프로세스 개발, 메뉴 테이블 설계 및 공통모듈화 담당 )',
             term: '개발기간 :  2018.11.20 ~ 2018.12.30'
-        }
+        },
+        {
+            pj: 'pj',
+            name: [''],
+            content: 'Real Time Sharing View (실시간 화면공유 협업 어플리케이션)',
+            tag: ['JAVA', 'Android', 'Firebase Realtime Database'],
+            git: 'https://github.com/KimNamKyu/RealTimeSharingView',
+            text: '캡스톤 졸업작품 실시간 화면 공유 협업시스템개발( 채팅시스템 및 디비연동 담당 )',
+            term: '개발기간 :  2018.03 ~ 2018.6'
+        },
     ]
 
     // onClick={() => window.open(item.url)}
@@ -38,19 +47,22 @@ function Project(props) {
     return (
         <Content style={{ background: '#fff', padding: "100px 50px 100px 50px", margin: '0 auto', width: '80%' }}>
             <div style={{ borderBottom: '2px dashed #e9e9e9' }}><h2 style={{ textAlign: 'center', fontSize: '38px' }}>Project</h2></div>
-            {/* <div style={{ position: 'relative', margin: '22px', paddingLeft: '20px', paddingRight: '40px', width: '50%' }}> */}
             {images.map((item, key) =>
                 <div>
-                    <Card title={item.content} headStyle={{fontSize:'20px'}} style={{ boxShadow: '5px 5px 5px 5px gray', margin: '22px', width: '100%'}} >
-                        <Slide transitionDuration={1000} style={{ marginBottom: '50px' }}>
-                            {item.name.map((name) =>
-                                <div style={{ textAlign: 'center' }}>
-                                    <Image src={require(`../img/${item.pj}/${name}.PNG`)} preview={false} style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', showQuickJumper: false }} onClick={() => window.open(item.git)} />
-                                </div>
-                            )}
-                        </Slide>
-                        <Meta avatar={<GithubOutlined onClick={() => {window.open(item.git)}} />} title={item.text} description={item.term} style={{fontSize:'16px', marginBottom:'20px'}}/>
-                        {item.tag.map(tag =><Tag>{tag}</Tag>)}
+                    <Card title={item.content} headStyle={{ fontSize: '20px' }} style={{ boxShadow: '5px 5px 5px 5px gray', margin: '22px auto', width: '80%' }} >
+                        {item.pj !== 'pj' ?
+                            <Slide transitionDuration={1000} style={{ marginBottom: '50px' }}>
+                                {item.name.map((name) =>
+                                    <div style={{ textAlign: 'center' }}>
+                                        <Image src={require(`../img/${item.pj}/${name}.PNG`)} preview={false} style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', showQuickJumper: false }} onClick={() => window.open(item.git)} />
+                                    </div>)}
+                            </Slide> :
+                            <div style={{ textAlign: 'center' }}>
+                                <Image src="error" preview={false} style={{ display: 'block', margin: '0 auto', minHeight: '340px' }} onClick={() => window.open(item.git)} />
+                            </div>}
+                        <Divider />
+                        <Meta avatar={<GithubOutlined onClick={() => { window.open(item.git) }} />} title={item.text} description={item.term} style={{ fontSize: '16px', marginBottom: '20px' }} />
+                        {item.tag.map(tag => <Tag>{tag}</Tag>)}
                     </Card>
                 </div>
             )}
--- src/Component/Skill.js
+++ src/Component/Skill.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Card, Image, Divider } from 'antd';
 
 function Skill(props) {
     const { Content } = Layout
+    const data = [
+        {
+            id: 'Backend',
+            list: ['']
+        },
+        {
+            id: 'Front',
+            list: ['']
+        },
+        {
+            id: 'DevOps',
+            list: ['']
+        },
+    ]
     return (
-        <Content style={{ height: '946px', background: '#fff', padding: "100px 50px 100px 50px", margin: '0 auto', width: '80%' }}>
-             <div style={{ borderBottom: '2px dashed #e9e9e9' }}><h2 style={{ textAlign: 'center', fontSize: '38px' }}>Skill</h2></div>
+        <Content style={{ height: '946px', background: '#fff', padding: "100px 50px 100px 50px", margin: '0 auto', width: '80%', position: 'relative' }}>
+            <div style={{ borderBottom: '2px dashed #e9e9e9' }}><h2 style={{ textAlign: 'center', fontSize: '38px' }}>Skill</h2></div>
+            {data.map((item, key) =>
+                <div>
+                    <Divider orientation="left">{item.id}</Divider>
+                </div>
+            )}
         </Content>
     );
 }
